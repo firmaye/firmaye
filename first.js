05b7f9edc947b6e66ee60e23ec5a726f0c7597b1
Firmaye <dadifirmaye@gmail.com>
2022-04-15 22:53:36 +0200

    first commit

---
+++ first.js
@@ -0,0 +1,11 @@
+function dropdownmenu(){
+    var x = document.getElementById("dropdownclick");
+    if(x.className === "nave"){
+      x.className += "responsive";
+    }
+    else{
+      x.className = "nave";
+    }
+    u=get
+  }
+  
